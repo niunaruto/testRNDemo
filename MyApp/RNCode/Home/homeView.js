a70b2578e28b25645fabac5facf34d01c4de0c32
niuNaruto <niuting604@163.com>
2018-11-16 15:14:28 +0800

    init

---
+++ MyApp/RNCode/Home/homeView.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+import React from 'react';
+import {
+    Animated,
+    Platform,
+    StyleSheet,
+    View,
+} from 'react-native';
+
+
+export default class homeView extends Component {
+    render() {
+        return (
+            <View style={{backgroundColor:'red',height:100,width:300}}>
+
+
+            </View>
+        );
+    }
+}
